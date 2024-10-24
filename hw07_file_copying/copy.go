6f5130369592c38105eedd6c98f9dbbce51d15e1
Kusmarov Alexandr <kusmarov_a@mail.ru>
2024-10-24 19:43:13 +0300

    hw07_file_copying HW_7 after review fix

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"errors"
+	"github.com/schollz/progressbar/v3"
 	"io"
 	"os"
-
-	"github.com/schollz/progressbar/v3"
+	"path/filepath"
 )
 
 var (
@@ -15,7 +14,7 @@ var (
 )
 
 func Copy(fromPath string, toPath string, offset, limit int64) error {
-	if fromPath == toPath {
+	if filepath.Abs(fromPath) == filepath.Abs(toPath) {
 		return ErrUnsupportedFile
 	}
 	special, err := isSpecialFile(fromPath)
@@ -36,11 +35,6 @@ func Copy(fromPath string, toPath string, offset, limit int64) error {
 
 	if offset > stat.Size() {
 		return ErrOffsetExceedsFileSize
-	}
-
-	err = limitCorrector(fromFile, &limit, &offset)
-	if err != nil {
-		return err
 	}
 
 	if limit == 0 || limit > stat.Size() {
@@ -70,46 +64,6 @@ func Copy(fromPath string, toPath string, offset, limit int64) error {
 	bar.Finish()
 
 	return nil
-}
-
-func limitCorrector(f *os.File, limit *int64, offset *int64) error {
-	reader := bufio.NewReader(f)
-	countR := int64(0)
-	curOffset := int64(0)
-	curLimit := int64(0)
-	var err error
-
-	// Подсчет и коррекция /r до оффсета
-	for curOffset < *offset+countR {
-		b, err := reader.ReadByte()
-		if err != nil {
-			break
-		}
-		curOffset++
-		if b == '\r' {
-			countR++
-		}
-	}
-
-	*offset = curOffset
-	countR = 0
-
-	// Подсчет и коррекция /r до лимита
-	for curLimit < *limit+countR {
-		b, err := reader.ReadByte()
-		if err != nil {
-			break
-		}
-		curLimit++
-		if b == '\r' {
-			countR++
-		}
-	}
-	*limit = curLimit
-
-	_, err = f.Seek(0, 0)
-
-	return err
 }
 
 func isSpecialFile(path string) (bool, error) {
