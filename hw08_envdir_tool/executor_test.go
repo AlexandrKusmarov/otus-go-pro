3c8c57c69ee1341c2f5f2f4e0e305734d47d7e7e
Kusmarov Alexandr <kusmarov_a@mail.ru>
2024-10-28 11:44:31 +0300

    hw08_envdir_tool HW_8 find bash

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -12,7 +13,11 @@ func TestRunCmd(t *testing.T) {
 	e, err := ReadDir("./testdata/env")
 	require.NoError(t, err)
 
-	fmt.Println(os.LookupEnv("SHELL"))
+	fmt.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+	env, _ := os.LookupEnv("SHELL")
+	fmt.Println("SHELL: " + env)
+	fmt.Println("ALL ENV" + strings.Join(os.Environ(), ","))
+	fmt.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 	code := RunCmd([]string{`/usr/local/bin/bash.exe`, "-c", "echo arg=1"}, e)
 	require.Equal(t, 0, code, "Exit code should be 0")
 }
