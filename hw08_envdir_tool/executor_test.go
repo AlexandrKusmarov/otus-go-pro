c50e11a12f75f171f1e6dba4d21978d8dc43771d
Kusmarov Alexandr <kusmarov_a@mail.ru>
2024-10-28 11:30:14 +0300

    hw08_envdir_tool HW_8 fix2

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -10,6 +10,6 @@ func TestRunCmd(t *testing.T) {
 	e, err := ReadDir("./testdata/env")
 	require.NoError(t, err)
 
-	code := RunCmd([]string{`$HOME/.bin`, "-c", "echo arg=1"}, e)
+	code := RunCmd([]string{`/usr/local/bin/bash.exe`, "-c", "echo arg=1"}, e)
 	require.Equal(t, 0, code, "Exit code should be 0")
 }
