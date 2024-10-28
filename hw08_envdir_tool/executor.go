c756cbd8de9edd0248878788b5f2dccacb837d32
Kusmarov Alexandr <kusmarov_a@mail.ru>
2024-10-28 11:16:04 +0300

    hw08_envdir_tool HW_8 implementation

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,47 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+const (
+	success = 0
+	failure = 1
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	com := exec.Command(cmd[0], cmd[1:]...) //nolint:gosec
+
+	// Установить переменные окружения
+	for k, v := range env {
+		err := os.Unsetenv(k)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error delete environment: %v\n", err)
+		}
+		if !v.NeedRemove {
+			err := os.Setenv(k, v.Value)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "error setting environment: %v\n", err)
+			}
+		}
+	}
+
+	com.Stderr = os.Stderr
+	com.Stdout = os.Stdout
+	com.Stdin = os.Stdin
+
+	// Запустить команду
+	if err := com.Run(); err != nil {
+		var exitErr *exec.ExitError
+		if errors.As(err, &exitErr) {
+			return exitErr.ExitCode()
+		}
+		fmt.Fprintf(os.Stderr, "error running command: %v\n", err)
+		return failure
+	}
+	return success
 }
