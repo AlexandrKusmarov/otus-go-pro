9e8ecd81107fa961b269cdfa87757a96b3210969
Kusmarov Alexandr <kusmarov_a@mail.ru>
2024-10-29 19:17:24 +0300

    hw08_envdir_tool HW_8 fix7

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bufio"
 	"errors"
+	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
@@ -60,6 +62,7 @@ func ReadlineFromFile(filename string) (EnvValue, error) {
 
 	envVal.NeedRemove = info.Size() == int64(0)
 
+	fmt.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ " + strconv.FormatInt(info.Size(), 10) + "~~~~~~~~ " + strconv.FormatBool(envVal.NeedRemove))
 	// Создаем новый Scanner
 	scanner := bufio.NewScanner(file)
 
