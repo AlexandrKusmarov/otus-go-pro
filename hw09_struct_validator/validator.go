71048a402f46b507b73aae7a2eafa878943c0411
Kusmarov Alexandr <kusmarov_a@mail.ru>
2024-11-20 13:22:28 +0300

    hw09_struct_validator HW_9 after review fix

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -33,144 +33,146 @@ func Validate(v interface{}) error {
 	}
 
 	var ve ValidationErrors
+	var err error
 
 	for i := 0; i < t.NumField(); i++ {
 		if vv.Field(i).CanInterface() && t.Field(i).Tag.Get("validate") != "" {
 			for _, rule := range strings.Split(t.Field(i).Tag.Get("validate"), "|") {
-				ve = applyOneRule(rule, t.Field(i).Name, vv.Field(i), ve)
+				ve, err = applyOneRule(rule, t.Field(i).Name, vv.Field(i), ve)
+				if err != nil {
+					return err
+				}
 			}
 		}
 	}
 	return ve
 }
 
-func applyOneRule(rule string, nameField string, dataForValidate reflect.Value, ves ValidationErrors) ValidationErrors {
+func applyOneRule(rule string, nameField string, dataForValidate reflect.Value,
+	ves ValidationErrors,
+) (ValidationErrors, error) {
 	rl := strings.SplitN(rule, ":", 2)
 	if len(rl) != 2 {
-		return append(ves, ValidationError{
-			nameField, fmt.Errorf("program error: Field %s -> invalid rule", nameField),
-		})
+		return nil, fmt.Errorf("program error: Field %s -> invalid rule", nameField)
 	}
 
 	switch dataForValidate.Kind() {
 	case reflect.String:
-		err := validateString(rl[0], rl[1], nameField, dataForValidate.String())
-		if !errors.Is(err.Err, nil) {
-			ves = append(ves, err)
+		validErr, err := validateString(rl[0], rl[1], nameField, dataForValidate.String())
+		if !errors.Is(validErr.Err, nil) {
+			ves = append(ves, validErr)
+			return ves, nil
 		}
+		return ves, err
 
 	case reflect.Int:
-		err := validateInt(rl[0], rl[1], nameField, dataForValidate.Int())
-		if !errors.Is(err.Err, nil) {
-			ves = append(ves, err)
+		validErr, err := validateInt(rl[0], rl[1], nameField, dataForValidate.Int())
+		if !errors.Is(validErr.Err, nil) {
+			ves = append(ves, validErr)
+			return ves, nil
 		}
-		return ves
+		return ves, err
 
 	case reflect.Slice:
+		var err error
 		for y := 0; y < dataForValidate.Len(); y++ {
-			ves = applyOneRule(rule, nameField+" element "+strconv.Itoa(y), dataForValidate.Index(y), ves)
+			ves, err = applyOneRule(rule, nameField+" element "+strconv.Itoa(y), dataForValidate.Index(y), ves)
+			if !errors.Is(err, nil) {
+				return ves, err
+			}
 		}
-		return ves
+		return ves, nil
 
 	default:
-		return append(ves, ValidationError{
-			nameField, fmt.Errorf("program error: Field %s -> type is not supported", nameField),
-		})
+		return nil, fmt.Errorf("program error: Field %s -> type is not supported", nameField)
 	}
-	return ves
 }
 
-func validateString(ruleName string, ruleValue string, nameField string, dataStrForValidator string) ValidationError {
+func validateString(ruleName string, ruleValue string, nameField string,
+	dataStrForValidator string,
+) (ValidationError, error) {
 	switch ruleName {
 	case "len":
 		rv, err := strconv.Atoi(ruleValue)
 		if err != nil {
-			return ValidationError{nameField, err}
+			return ValidationError{}, err
 		}
 		if len(dataStrForValidator) != rv {
 			return ValidationError{
 				nameField,
 				fmt.Errorf("validation error: Field %s -> length must be %s", nameField, ruleValue),
-			}
+			}, nil
 		}
 	case "regexp":
 		re, err := regexp.Compile(ruleValue)
 		if err != nil {
-			return ValidationError{
-				nameField,
-				fmt.Errorf("program error: Field %s -> invalid regexp pattern", nameField),
-			}
+			return ValidationError{}, fmt.Errorf("program error: Field %s -> invalid regexp pattern", nameField)
 		}
 		if !re.MatchString(dataStrForValidator) {
 			return ValidationError{
 				nameField,
 				fmt.Errorf("validation error: Field %s -> regexp does not match %s", nameField, dataStrForValidator),
-			}
+			}, nil
 		}
 	case "in":
 		values := strings.Split(ruleValue, ",")
 		for _, v := range values {
 			if dataStrForValidator == v {
-				return ValidationError{nameField, nil}
+				return ValidationError{nameField, nil}, nil
 			}
 		}
 		return ValidationError{
 			nameField,
 			fmt.Errorf("validation error: Field %s -> must be one of %s", nameField, ruleValue),
-		}
+		}, nil
 	default:
-		return ValidationError{
-			nameField,
-			fmt.Errorf("program error: Field %s -> unknown rule %s", nameField, ruleName),
-		}
+		return ValidationError{}, fmt.Errorf("program error: Field %s -> unknown rule %s", nameField, ruleName)
 	}
-	return ValidationError{nameField, nil}
+	return ValidationError{nameField, nil}, nil
 }
 
-func validateInt(ruleName string, ruleValue string, nameField string, dataIntForValidator int64) ValidationError {
+func validateInt(ruleName string, ruleValue string, nameField string,
+	dataIntForValidator int64) (ValidationError, error) {
 	switch ruleName {
 	case "min":
 		rv, err := strconv.ParseInt(ruleValue, 10, 64)
 		if err != nil {
-			return ValidationError{nameField, err}
+			return ValidationError{}, fmt.Errorf("program error: Field %s -> invalid parse int", nameField)
 		}
 		if dataIntForValidator < rv {
 			return ValidationError{
 				nameField,
 				fmt.Errorf("validation error: Field %s -> must be >= %s", nameField, ruleValue),
-			}
+			}, nil
 		}
 	case "max":
 		rv, err := strconv.ParseInt(ruleValue, 10, 64)
 		if err != nil {
-			return ValidationError{nameField, err}
+			return ValidationError{}, fmt.Errorf("program error: Field %s -> invalid parse int", nameField)
 		}
 		if dataIntForValidator > rv {
 			return ValidationError{
 				nameField,
 				fmt.Errorf("validation error: Field %s -> max is %s", nameField, ruleValue),
-			}
+			}, nil
 		}
 	case "in":
 		values := strings.Split(ruleValue, ",")
 		for _, v := range values {
 			vInt, err := strconv.ParseInt(v, 10, 64)
 			if err != nil {
-				return ValidationError{nameField, err}
+				return ValidationError{}, fmt.Errorf("program error: Field %s -> invalid parse int", nameField)
 			}
 			if dataIntForValidator == vInt {
-				return ValidationError{nameField, nil}
+				return ValidationError{nameField, nil}, nil
 			}
 		}
 		return ValidationError{
 			nameField,
 			fmt.Errorf("validation error: Field %s -> must be one of %s ", nameField, ruleValue),
-		}
+		}, nil
 	default:
-		return ValidationError{
-			nameField,
-			fmt.Errorf("validation error: Field %s -> unknown rule %s ", nameField, ruleName),
-		}
+		return ValidationError{}, fmt.Errorf("program error: Field %s -> unknown rule %s ", nameField, ruleName)
 	}
-	return ValidationError{nameField, nil}
+	return ValidationError{nameField, nil}, nil
 }
