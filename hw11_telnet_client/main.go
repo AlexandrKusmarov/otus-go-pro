9d24981e10ac147592f02321da0a5955e2c6402a
Kusmarov Alexandr <kusmarov_a@mail.ru>
2024-12-02 12:54:14 +0300

    hw11_telnet_client HW 11 implementation

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,37 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	if len(os.Args) < 3 {
+		fmt.Fprintln(os.Stderr, "Usage: go-telnet host port")
+		os.Exit(1)
+	}
+
+	address := fmt.Sprintf("%s:%s", os.Args[1], os.Args[2])
+	client := NewTelnetClient(address, 10*time.Second, os.Stdin, os.Stdout)
+
+	if err := client.Connect(); err != nil {
+		fmt.Fprintf(os.Stderr, "Connection error: %v\n", err)
+		os.Exit(1)
+	}
+	defer client.Close()
+
+	fmt.Fprintf(os.Stderr, "...Connected to %s\n", address)
+
+	go func() {
+		if err := client.Receive(); err != nil {
+			fmt.Fprintf(os.Stderr, "Receive error: %v\n", err)
+			os.Exit(1)
+		}
+	}()
+
+	if err := client.Send(); err != nil {
+		fmt.Fprintf(os.Stderr, "Send error: %v\n", err)
+	}
+	fmt.Fprintln(os.Stderr, "...Connection closed")
 }
